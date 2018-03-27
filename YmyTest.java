98b507a84f29b9d59de328a224c87f27d5d715c0
Can-yao TANG <ctangaf@connect.ust.hk>
2018-03-27 16:44:38 +0800

    ymytest

--- YmyTest.java
+++ YmyTest.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 
 import jdbm.RecordManager;
 import jdbm.RecordManagerFactory;
+import jdbm.helper.FastIterator;
 import jdbm.htree.HTree;
 
 import java.io.BufferedReader;  
@@ -11,26 +12,68 @@ import java.io.FileWriter;
 import java.io.IOException;  
   
 public class YmyTest {  
-	public void printdash(BufferedWriter out)
+	public static void printdash(BufferedWriter out)
 	{
 	 try {
-		out.write("-------------------------------------------------------------------------------------------");
+		out.write("-------------------------------------------------------------------------------------------\r\n");
 	} catch (IOException e) {
 		e.printStackTrace();
 	}	
+	 
 	}
     public static void main(String args[]) {  
         try { 
-			InvertedIndex Id_Url_index = new InvertedIndex("project","id_url");
-			InvertedIndex Id_Title_index = new InvertedIndex("project","id_title");
-			InvertedIndex Id_ContentLength_index = new InvertedIndex("project","id_contentlength");
-			InvertedIndex Id_LastModified_index = new InvertedIndex("project","id_lastmodified");
-			InvertedIndex ChildLink_index = new InvertedIndex("project","childlink");
-			InvertedIndex ParentLink_index = new InvertedIndex("project","parentlink");
+
+        	Launcher launcher=new Launcher();
+        	//Launcher.Id_Title_index.printAll();
             File file = new File("spider_result.txt");    
             file.createNewFile();   
-            BufferedWriter out = new BufferedWriter(new FileWriter(file));  
-            out.write("Testing\r\n");  
+            BufferedWriter out = new BufferedWriter(new FileWriter(file));
+            
+            String keyTi;
+            String keyUr;
+            String keyCl;
+            String keyLm;
+            String keyCi;
+            FastIterator iterTi = Launcher.Id_Title_index.hashtable.keys();
+            FastIterator iterUr = Launcher.Id_Url_index.hashtable.keys();
+            FastIterator iterCl = Launcher.Id_ContentLength_index.hashtable.keys();
+            FastIterator iterLm = Launcher.Id_LastModified_index.hashtable.keys();
+            FastIterator iterCi = Launcher.ChildLink_index.hashtable.keys();
+
+            for(int i=0;i<30;i++)
+            {
+            keyTi=(String)iterTi.next();
+            keyUr=(String)iterUr.next();
+            keyCl=(String)iterCl.next();
+            keyLm=(String)iterLm.next();
+            out.write(Launcher.Id_Title_index.hashtable.get(keyTi)+"\r\n");
+            out.write(Launcher.Id_Url_index.hashtable.get(keyUr)+"\r\n");
+            if((String)Launcher.Id_LastModified_index.hashtable.get(keyLm)==" 0")
+            out.write("unknown,");
+            else
+            out.write(Launcher.Id_LastModified_index.hashtable.get(keyLm)+",");
+            if((String)Launcher.Id_ContentLength_index.hashtable.get(keyCl)==" -1")
+            out.write("unknown\r\n");
+            else
+            out.write(Launcher.Id_ContentLength_index.hashtable.get(keyCl)+"\r\n");
+            
+            while((keyCi=(String)iterCi.next()) != null)
+            {
+            String[] tokens = ((String) Launcher.ChildLink_index.hashtable.get(keyCi)).split(";");
+            for(String token:tokens)
+            {
+            	token=token.replaceAll(" ", ""); 
+                out.write(Launcher.Id_Url_index.hashtable.get((String)token)+"\r\n");
+            }
+            }
+            
+            printdash(out);
+            //out.write(Launcher, off, len);
+            }
+
+
+            //out.write("Testing\r\n");  
             out.flush();  
             out.close();  
         } catch (Exception e) {  
